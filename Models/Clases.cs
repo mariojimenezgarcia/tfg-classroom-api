1d73d865ba9c6e76c663c4b17123df4fd0872275
mariojimenezgarcia <mario95201@gmail.com>
2025-06-04 18:57:48 +0200

    notas

--- Models/Clases.cs
+++ Models/Clases.cs
@@ -119,5 +119,39 @@ namespace apiClassroom.Models
             // Lista de errores (vacía si todo OK)
             public List<Error> error { get; set; } = new List<Error>();
         }
+        public class PonerNotaRequest
+        {
+            public string token { get; set; }
+            public int idEntrega { get; set; }
+            public decimal nota { get; set; } // o int, según tu diseño; aquí uso decimal por si quieres notas con decimales
+        }
+
+        // 2. Response para poner nota
+        public class PonerNotaResponse
+        {
+            public string mensaje { get; set; }
+            public List<Error> error { get; set; } = new List<Error>();
+        }
+
+        // Tu clase Error ya existente:
+        public class VerNotasRequest
+        {
+            public string token { get; set; }
+        }
+
+        // 2. DTO para cada fila de “ver notas”
+        public class NotaData
+        {
+            public string titulo { get; set; }
+            public decimal nota { get; set; }
+            public string nombreUsuario { get; set; }
+        }
+
+        // 3. Response para ver notas
+        public class VerNotasResponse
+        {
+            public List<NotaData> notas { get; set; } = new List<NotaData>();
+            public List<Error> error { get; set; } = new List<Error>();
+        }
     }
 }
